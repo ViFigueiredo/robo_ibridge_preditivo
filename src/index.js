04101e22b88ef052c766d4d35a30f6e8fe27e12b
Vinicius Figueiredo <vifigcode@gmail.com>
2023-06-22 16:45:29 -0300

    ...

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ const testConnection = require('./connection');
 const consumoAPI = require('./api');
 const convertDataApi = require('./convert');
 
-// Agenda a tarefa para ser executada todos os dias às 20:00 (exceto sábados e domingos)
-cron.schedule('00 20 * * *', async () => {
+console.log('Aguardando inicio da aplicação...');
+
+async function App() {
   try {
     await testConnection(database);
     const dataAPI = await consumoAPI();
     await convertDataApi(dataAPI);
-    await database.close();
+    //await database.close();
   } catch (error) {
     console.error('Ocorreu um erro:', error);
   }
-});
+}
+
+//App();
+
+// Agenda a tarefa para ser executada todos os dias às 20:00 (exceto sábados e domingos)
+cron.schedule('00 20 * * *', () => { App() });
